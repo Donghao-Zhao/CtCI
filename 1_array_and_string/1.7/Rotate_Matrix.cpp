2caddac28ba2b9ef9df9fc58220fd15d2908bc41
Donghao <tony.donghao@gmail.com>
2023-01-09 10:19:32 -0800

    1.7

--- 1_array_and_string/1.7/Rotate_Matrix.cpp
+++ 1_array_and_string/1.7/Rotate_Matrix.cpp
@@ -4,21 +4,26 @@
 
 #include "Rotate_Matrix.h"
 
-#define DIM 5
+#define COLUMN 5
+#define END_INDEX (COLUMN - 1)
 
 namespace CtCI {
     bool myString_1_7::rotateMatrix(int ** matrix) {
         int tmp = 0;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                cout << *((int *)matrix + i*5 + j) << endl;
+        for (int i = 0; i != COLUMN / 2; i++) {
+            for (int j = i; j != (COLUMN - 1 - i); j++) {
+                tmp = *((int *)matrix + i * COLUMN + j);   //top to tmp
+                *((int *)matrix + i * COLUMN + j) = *((int *)matrix + (END_INDEX - j) * COLUMN + i);    // top <-- left
+                *((int *)matrix + (END_INDEX - j) * COLUMN + i) = *((int *)matrix + COLUMN * (END_INDEX - i) + (END_INDEX - j)); // left <-- bot
+                *((int *)matrix + COLUMN * (END_INDEX - i) + (END_INDEX - j)) = *((int *)matrix + COLUMN * j + (END_INDEX - i));    // bot <-- right
+                *((int *)matrix + COLUMN * j + (END_INDEX - i)) = tmp; // right <-- tmp
             }
         }
         return false;
     }
 
     int myString_1_7::unitTest() {
-        int units[DIM][DIM] = {
+        int units[COLUMN][COLUMN] = {
                 1, 2, 3, 4, 5,
                 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15,
@@ -30,8 +35,9 @@ namespace CtCI {
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                cout << *((int *)units + i*5 + j) << endl;
+                cout << *((int *)units + i * COLUMN + j) << " ";
             }
+            cout << endl;
         }
 
         return 0;
